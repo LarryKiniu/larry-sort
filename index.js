82d7fb3ee2e71d50b7d4ee529c0ac9defe467956
Larry Kiniu <ngethelawrence@gmail.com>
2022-01-24 23:15:17 +0300

    add more tests from notebook

--- index.js
+++ index.js
@@ -37,10 +37,12 @@ function sort(array){
 let numbers0 = [13,1,90,102,4,7,4,59,100,201,3,77,20,62];
 let numbers1 = [593,793,623,26,1990,31,27,16,6];
 let numbers2 = [34,56,25,67,9];
-let numbers3 = [3,30,0,100,33,27,4,58,70,10,9,101,17,82,16,22,76]
+let numbers3 = [3,30,0,100,33,27,4,58,70,10,9,101,17,82,16,22,76];
+let numbers4 = [2,6,4,14,11,13,7,16,5,9,1,10,8,12,3,15];
 
 // sort the myArray of numbers 
 console.log('numbers0',walkGrid(sort(numbers0)));
 console.log('numbers1',walkGrid(sort(numbers1)));
 console.log('numbers2',walkGrid(sort(numbers2)));
 console.log('numbers3',walkGrid(sort(numbers3)));
+console.log('numbers4',walkGrid(sort(numbers4)));
